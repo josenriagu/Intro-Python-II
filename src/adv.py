5a8d51530f2d3c79d36ad23dddd58de959abe7b0
Josemaria Nriagu <49484425+josenriagu@users.noreply.github.com>
2020-02-13 10:35:14 +0100

    feat(python): make player move across rooms - move in the chosen direction if permitted - print error if direction is not permitted - quit if input is q

--- src/adv.py
+++ src/adv.py
@@ -1,51 +1,109 @@
 from room import Room
+from player import Player
 
 # Declare all the rooms
 
-room = {
-    'outside':  Room("Outside Cave Entrance",
-                     "North of you, the cave mount beckons"),
-
-    'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty
-passages run north and east."""),
-
-    'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling
-into the darkness. Ahead to the north, a light flickers in
-the distance, but there is no way across the chasm."""),
-
-    'narrow':   Room("Narrow Passage", """The narrow passage bends here from west
-to north. The smell of gold permeates the air."""),
-
-    'treasure': Room("Treasure Chamber", """You've found the long-lost treasure
-chamber! Sadly, it has already been completely emptied by
-earlier adventurers. The only exit is to the south."""),
+rooms = {
+    'outside':  Room(
+        "Outside Cave Entrance",
+        "North of you, the cave mount beckons"
+    ),
+    'foyer':    Room(
+        "Foyer",
+        """Dim light filters in from the south. Dusty passages run north and east."""
+    ),
+    'overlook': Room(
+        "Grand Overlook",
+        """A steep cliff appears before you, falling into the darkness. Ahead to the north, a light flickers in the distance, but there is no way across the chasm."""
+    ),
+    'narrow':   Room(
+        "Narrow Passage",
+        """The narrow passage bends here from west to north. The smell of gold permeates the air."""
+    ),
+    'treasure': Room(
+        "Treasure Chamber",
+        """You've found the long-lost treasure chamber! Sadly, it has already been completely emptied by earlier adventurers. The only exit is to the south."""
+    ),
 }
 
 
 # Link rooms together
 
-room['outside'].n_to = room['foyer']
-room['foyer'].s_to = room['outside']
-room['foyer'].n_to = room['overlook']
-room['foyer'].e_to = room['narrow']
-room['overlook'].s_to = room['foyer']
-room['narrow'].w_to = room['foyer']
-room['narrow'].n_to = room['treasure']
-room['treasure'].s_to = room['narrow']
+rooms['outside'].n_to = rooms['foyer']
+rooms['foyer'].s_to = rooms['outside']
+rooms['foyer'].n_to = rooms['overlook']
+rooms['foyer'].e_to = rooms['narrow']
+rooms['overlook'].s_to = rooms['foyer']
+rooms['narrow'].w_to = rooms['foyer']
+rooms['narrow'].n_to = rooms['treasure']
+rooms['treasure'].s_to = rooms['narrow']
 
 #
 # Main
 #
 
 # Make a new player object that is currently in the 'outside' room.
-
+start_room = 'outside'
 # Write a loop that:
 #
-# * Prints the current room name
-# * Prints the current description (the textwrap module might be useful here).
-# * Waits for user input and decides what to do.
+# * Prints the current room name - ✔️
+# * Prints the current description (the textwrap module might be useful here) - ✔️
+# * Waits for user input and decides what to do - ✔️
 #
-# If the user enters a cardinal direction, attempt to move to the room there.
-# Print an error message if the movement isn't allowed.
+# If the user enters a cardinal direction, attempt to move to the room there - ✔️
+# Print an error message if the movement isn't allowed - ✔️
 #
-# If the user enters "q", quit the game.
+# If the user enters "q", quit the game - ✔️
+
+player = Player(rooms[start_room])
+all_directions = ['N', 'E', 'W', 'S']
+room_directions = []
+quit_game = False
+
+
+def main():
+    global room_directions
+    while not quit_game:
+        # print room information
+        player.print_room()
+        # get permitted directions for the current room
+        room_directions = [key.split("_")[0].upper() for key, val in vars(
+            player.current_room).items() if key not in ['name', 'description', 'key']]
+        # inform player of available directions
+        print()
+        print(f'All Coordinates: {all_directions}',
+              f'This room permits: {room_directions}', sep="\n")
+        # allow player input
+        print()
+        user_input = input(
+            "Enter a direction to travel or 'q' to quit game\n").upper()
+        # make a journey
+        travel(user_input)
+
+
+def travel(user_input):
+    # check if directions are pernitted
+    if user_input == 'N' and user_input in room_directions:
+        player.move(player.current_room.n_to)
+    elif user_input == 'E' and user_input in room_directions:
+        player.move(player.current_room.e_to)
+    elif user_input == 'W' and user_input in room_directions:
+        player.move(player.current_room.w_to)
+    elif user_input == 'S' and user_input in room_directions:
+        player.move(player.current_room.s_to)
+    elif user_input == 'Q':
+        quit(player.current_room.name)
+    elif user_input in all_directions and user_input not in room_directions:
+        print("Auch! you can travel to this direction from this room")
+    elif user_input not in all_directions:
+        print("Errhm! Are you just turnioniown? Choose from all coordinates!")
+
+
+def quit(room_name):
+    global quit_game
+    print(f'You are quitting the game from {room_name}', 'Bye!', sep="\n")
+    quit_game = True
+
+
+# start game
+main()
--- src/player.py
+++ src/player.py
@@ -1,2 +1,22 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+from textwrap import TextWrapper
+
+
+class Player:
+    def __init__(self, current_room):
+        self.current_room = current_room
+
+    def print_room(self):
+        print()  # prints empty line for demarcation
+        print("You are currently in")
+        print("Room name: {}".format(self.current_room.name))
+        # using textwrap to break long room descriptions
+        wrapper = TextWrapper(width=55)
+        word_list = wrapper.wrap(text=self.current_room.description)
+        print("Room description: ", end=" ")
+        for line in word_list:
+            print(line)
+
+    def move(self, next_room):
+        self.current_room = next_room
